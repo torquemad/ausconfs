3ee7b17d2eb3f5e7b9a6b5b1db030aa800b802a1
Louis Moore <burntliketoast@gmail.com>
2016-03-30 22:43:16 +1100

    create user and conference association

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,8 +1,9 @@
 class Conference < ActiveRecord::Base
   belongs_to :user
 
+  validates :user_id, presence: true
   validates :name, presence: true, length: { minimum: 1, maximum: 100 }
-  validates :date, presence: true
   validates :summary, presence: true, length: { minimum: 1, maximum: 300 }
   validates :description, presence: true, length: { minimum: 1, maximum: 2000 }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160326101904) do
+ActiveRecord::Schema.define(version: 20160330072131) do
 
   create_table "conferences", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160326101904) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
