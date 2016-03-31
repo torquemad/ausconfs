588f4f39499d4f145fcffdb430c8c8f2e2f31740
Louis Moore <burntliketoast@gmail.com>
2016-03-31 18:14:24 +1100

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
+
   resources :conferences
-	root "pages#home"
+
+  root "pages#home"
+
+  get '/home', to: 'pages#home'
+
+  get '/conferences', to: 'conferences#index'
+  get '/conferences/new', to: 'conferences#new'
+  post '/conferences', to: 'conferences#create'
+  get '/conferences/:id/edit', to: 'conferences#edit'
+  patch '/conferences/:id', to: 'conferences#update'
+  get 'conferences/:id', to: 'conferences#show'
+  delete '/conferences/:id', to: 'conferences#destroy'
   
 end
