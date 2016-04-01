bd7414e5c186baa4b75d7af5aa20a8436bcc5198
Louis Moore <burntliketoast@gmail.com>
2016-04-01 17:37:34 +1100

    removed duplicating and redundant routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :conferences
 
   root "pages#home"
-
   get '/home', to: 'pages#home'
-
-  get '/conferences', to: 'conferences#index'
-  get '/conferences/new', to: 'conferences#new'
-  post '/conferences', to: 'conferences#create'
-  get '/conferences/:id/edit', to: 'conferences#edit'
-  patch '/conferences/:id', to: 'conferences#update'
-  get 'conferences/:id', to: 'conferences#show'
-  delete '/conferences/:id', to: 'conferences#destroy'
+  
+  resources :conferences
   
 end
