a177e2a1b68bf01c57e77f9c9b5ea6f6c3066342
Louis Moore <burntliketoast@gmail.com>
2016-04-01 17:39:12 +1100

    conferences controller

---
+++ app/controllers/conferences_controller.rb
@@ -0,0 +1,47 @@
+class ConferencesController < ApplicationController
+
+  def index
+    @conferences = Conference.all
+  end
+
+	def show
+		@conference = Conference.find params[:id]
+	end
+
+	def new
+    @conference = Conference.new
+	end
+
+  def create
+    @conference = Conference.new(conference_params)
+    @conference.user = User.find(1) #until auth implemented
+
+    if @conference.save
+      flash[:success] = "Your conference has successfully been created."
+      redirect_to conferences_path
+    else
+      render :new
+    end
+  end
+
+	def edit
+    @conference = Conference.find params[:id]
+	end
+
+	def update
+    @conference = Conference.find params[:id]
+    if @conference.update(conference_params)
+      flash[:success] = "Your conference #{@conference.name} has successfully been updated."
+      redirect_to conference_path(@conference)
+    else
+      render :edit
+    end
+	end
+
+  private
+    
+  def conference_params
+    params.require(:conference).permit(:name, :date, :summary, :description, :url)
+  end
+
+end
