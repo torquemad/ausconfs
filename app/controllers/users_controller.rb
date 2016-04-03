11e03ac70ce099b85652196e2c9dd12dbac796f2
Louis Moore <burntliketoast@gmail.com>
2016-04-03 20:14:50 +1000

    create method for user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,23 @@ class UsersController < ApplicationController
   end
  
   def create
- 
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Account created"
+      redirect_to conferences_path
+    else
+      render 'new'
+    end
   end
 
   def update
 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
