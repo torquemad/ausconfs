3821619955af3594610cb11b23f1eb8e1d0fb3e6
Louis Moore <burntliketoast@gmail.com>
2016-04-03 12:58:04 +1000

    mount picture uploader and size validation

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -5,5 +5,16 @@ class Conference < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 1, maximum: 100 }
   validates :summary, presence: true, length: { minimum: 1, maximum: 300 }
   validates :description, presence: true, length: { minimum: 1, maximum: 2000 }
+  validate :image_size
+
+  mount_uploader :picture, PictureUploader
+
+  private
+
+  def image_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "Image size too large (over 5mb)")
+    end
+  end
 
 end
