b5742261dde2feb9d54c1826a93060f772dd8884
Louis Moore <burntliketoast@gmail.com>
2016-04-04 17:44:19 +1000

    before actions for require_user and set_conference

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -1,11 +1,13 @@
 class ConferencesController < ApplicationController
+  before_action :set_conference, only: [:show, :edit, :update]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_identical_user, only: [:edit, :update]
 
   def index
     @conferences = Conference.all
   end
 
 	def show
-		@conference = Conference.find params[:id]
 	end
 
 	def new
@@ -14,22 +16,20 @@ class ConferencesController < ApplicationController
 
   def create
     @conference = Conference.new(conference_params)
-    @conference.user = User.find(2) #until auth implemented
+    @conference.user = current_user 
 
     if @conference.save
       flash[:success] = "Your conference has successfully been created."
-      redirect_to conferences_path
+      redirect_to conferences_path(current_user)
     else
       render :new
     end
   end
 
 	def edit
-    @conference = Conference.find params[:id]
 	end
 
 	def update
-    @conference = Conference.find params[:id]
     if @conference.update(conference_params)
       flash[:success] = "Your conference #{@conference.name} has successfully been updated."
       redirect_to conference_path(@conference)
@@ -42,6 +42,17 @@ class ConferencesController < ApplicationController
     
   def conference_params
     params.require(:conference).permit(:name, :date, :summary, :description, :url)
+  end
+
+  def set_conference
+		@conference = Conference.find params[:id]
+  end
+
+  def require_identical_user
+    if current_user != @conference.user
+      flash[:danger] = "You cannot edit this"
+      redirect_to conference_path
+    end
   end
 
 end
