246c8eed93958359821501117b1504c98ee689a4
Louis Moore <burntliketoast@gmail.com>
2016-04-04 11:58:08 +1000

    forgot render method
    things

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_recipe_app_session'
+Rails.application.config.session_store :cookie_store, key: '_conference_app_session'
