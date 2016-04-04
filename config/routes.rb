960f54a04fea825c078c7d3ae88a28e902455774
Louis Moore <burntliketoast@gmail.com>
2016-04-04 17:42:57 +1000

    things

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,9 @@ Rails.application.routes.draw do
 
   get '/register', to: 'users#new'
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
   
 end
